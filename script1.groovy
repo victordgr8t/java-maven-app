86fab8717fb1274fae795088a52125720105d285
Raven R <rivo099@gmail.com>
2024-08-03 10:10:11 +0000

    Update script1.groovy

--- script1.groovy
+++ script1.groovy
@@ -1,8 +1,18 @@
-stage("build jar") {
-            steps {
-                script {
-                    echo "building jar"
-                    sh 'mvn package'
-                }
-            }
-        }
+def buildJar() {
+    echo 'building the application...'
+    sh 'mvn package'
+}
+
+def buildImage() {
+    echo "building the docker image"
+                    withCredentials([usernamePassword(credentialsId: 'docker-hub-repo', passwordVariable: 'PASS', usernameVariable: 'USER')]){
+                        sh 'docker build -t ravenravel/demo-app:jma-2.0 .'
+                        sh 'echo $PASS | docker login -u $USER --password-stdin'
+                        sh 'docker push ravenravel/demo-app:jma-2.0'
+                    }
+}
+
+def deployApp() {
+    echo 'deploying the application..'
+}
+return this
