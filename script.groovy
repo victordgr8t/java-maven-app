9e375b191d0fbbb4010b5dc4d7e4331f36cf91b5
Nnamdi Victor Modebe <63635704+victordgr8t@users.noreply.github.com>
2025-04-10 12:28:49 -0400

    Update script.groovy

--- script.groovy
+++ script.groovy
@@ -1,16 +1,3 @@
-def buildJar () {
-    echo 'building the application...'
-    sh 'mvn package'
-}
-
-def buildImage () {
-    echo 'building the the docker image...'
-    withCredentials([usernamePassword(credentialsId: 'DOCKER-LOGIN', passwordVariable: 'PASS', usernameVariable: 'USER')]) {
-        sh 'docker build -t vicdg8t/my-repo:jma-2.0 .'
-        sh "echo $PASS | docker login -u $USER --password-stdin"
-        sh 'docker push vicdg8t/my-repo:jma-2.0'
-    }
-}
 
 def testApp() {
     echo 'testing the application...'
